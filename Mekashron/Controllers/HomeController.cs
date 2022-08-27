4fb2d6749adc185c459890453742252aa8237fe6
Bakhtiyar Amrahov <bakhtiyaria@code.edu.az>
2022-08-27 04:11:47 +0400

    firstCommit

---
+++ Mekashron/Controllers/HomeController.cs
@@ -0,0 +1,51 @@
+﻿using Mekashron.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace Mekashron.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            LoginVM loginVM = new LoginVM();
+            return View(loginVM);
+        }
+
+        
+
+        public IActionResult Login()
+        {
+            LoginVM loginVM = new LoginVM();
+            return View(loginVM);
+        }
+
+        //public IActionResult Login(MekashronLogin.LoginRequest request)
+
+        //{
+        //    LoginVM loginVM = new LoginVM()
+        //    {
+        //        Name = request.UserName,
+        //        Password = request.Password,
+        //        LoginResult = "success"
+
+        //    };
+        //    return View(loginVM);
+        //}
+
+        [HttpPost]
+        public IActionResult Login(string name, string password)
+        {
+            MekashronLogin.ICUTechClient request = new MekashronLogin.ICUTechClient();
+            Task<MekashronLogin.LoginResponse> result = request.LoginAsync(name, password, "3151");
+
+
+            MekashronLogin.LoginResponse loginResponse = result.Result;
+            LoginVM loginVM = new LoginVM()
+            {
+                Result = result.Result.@return,
+            };
+            return View(loginVM);
+        }
+    }
+}
---
+++ Mekashron/Models/LoginVM.cs
@@ -0,0 +1,9 @@
+﻿namespace Mekashron.Models
+{
+    public class LoginVM
+    {
+        public string Name { get; set; }
+        public string Password { get; set; }
+        public string Result { get; set; }
+    }
+}
